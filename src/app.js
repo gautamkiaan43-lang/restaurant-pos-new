2d67aeab97a50f1d07ca12b6ece5100e26c86637
gautamkiaan43-lang <gautamkiaan43@gmail.com>
2026-06-13 15:47:30 +0530

    Update app.js
    zxc

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ app.use(cors({
     'http://localhost:3000', 
     'http://127.0.0.1:5173',
     'https://restorentposback.softwaredemolive.live',
+    'https://posrestorant.softwaredemolive.live',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
